9622fcbe1bad5ce25f27fb94b880fb4671f80d45
jacopo-derrico <derricoj.design@gmail.com>
2024-02-26 16:19:19 +0100

    add bootstrap w/ table

--- index.php
+++ index.php
@@ -43,25 +43,57 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel Search</title>
+
+    <!-- Bootstarp 5.3.3 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <?php   foreach ($hotels as $hotel) {
-        echo "{$hotel['name']}
-                <br>
-                {$hotel['description']}
-                <br>
-                {$hotel['parking']}
-                <br>
-                {$hotel['vote']}
-                <br>
-                {$hotel['distance_to_center']}
-            ";
-    };
-    ?>
+   <div class="container">
+        <div class="row m-auto my-5">
+            <div class="col-8 m-auto ">
+                <h1 class="h1 text-center text-white mb-5 ">Hotels review</h1>
+                <table class="table table-dark table-hover table-bordered">
+                    <thead>
+                        <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Parking</th>
+                        <th scope="col">Vote</th>
+                        <th scope="col">From center</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        <?php foreach ($hotels as $hotel) : ?>
+                        <tr>
+                            <th scope="row">
+                                <?= $hotel['name'] ?>
+                            </th>
+                            <td>
+                                <?= $hotel['description'] ?>
+                            </td>
+                            <td>
+                                <?= ($hotel['parking']) ? 'Si' : 'No' ?>  
+                            </td>
+                            <td>
+                                <?= $hotel['vote'] ?>  
+                            </td>
+                            <td>
+                                <?= "{$hotel['distance_to_center']} km" ?>  
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+   </div>
+
+    <!-- Bootstarp 5.3.3 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
